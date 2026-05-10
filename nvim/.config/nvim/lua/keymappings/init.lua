d167e5549ea3ed7ee67a3a42cb78af2ffbd585f7
Andreas Röhrle-Förnzler <andreas.roehrle@web.de>
2026-05-10 07:58:35 +0200

    Minor changes to keymappings

--- nvim/.config/nvim/lua/keymappings/init.lua
+++ nvim/.config/nvim/lua/keymappings/init.lua
@@ -1,8 +1,8 @@
 --
 -- Basic keymaps
 --
-vim.keymap.set("n", "<leader>w", ":w<CR>", {desc = "save"})
+vim.keymap.set("n", "<leader>w", ":w<CR>", {desc = "write"})
 vim.keymap.set("n", "<leader>q", ":q<CR>", {desc = "quit"})
 
-vim.keymap.set("n", "<leader>ex", vim.cmd.NvimTreeFocus, {desc = "Focus NvimTree"})
+vim.keymap.set("n", "<leader>e", vim.cmd.NvimTreeFocus, {desc = "Focus NvimTree"})
 
